ca3e3a03fd28a2b8f4ef5b2a41310a0b1fc616a4
FauzyMaulana <beanonym04@gmail.com>
2025-02-18 22:35:49 +0700

    added new class setup base color

---
+++ lib/core/config/theme/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFE21221);
+  static const Color primaryDark = Color(0xFF002171);
+  static const Color primaryLight = Color(0xFF5472D3);
+  static const Color secondary = Color(0xFFFFFFFF);
+  static const Color secondaryDark = Color(0xFFC7C7C7);
+  static const Color accent = Color(0xFFAAAAAA);
+  static const Color background = Color(0xFFE0E0E0);
+  static const Color backgroundDark = Color(0xFF181A20);
+  static const Color surface = Color(0xFFFFFFFF);
+  static const Color error = Color(0xFFB00020);
+  static const Color onPrimary = Color(0xFFFFFFFF);
+  static const Color onSecondary = Color(0xFFE21221);
+  static const Color onBackground = Color(0xFF000000);
+  static const Color onSurface = Color(0xFF000000);
+  static const Color onError = Color(0xFFFFFFFF);
+}
