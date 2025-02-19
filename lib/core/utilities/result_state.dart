9b97290801f282c9533b0af93e13af72667984ba
FauzyMaulana <beanonym04@gmail.com>
2025-02-19 16:43:10 +0700

    added sealed result state

---
+++ lib/core/utilities/result_state.dart
@@ -0,0 +1,17 @@
+sealed class ResultState<T> {
+  const ResultState();
+
+  get data => null;
+}
+
+class Loading<T> extends ResultState<T> {}
+
+class Success<T> extends ResultState<T> {
+  final T data;
+  Success(this.data);
+}
+
+class Error<T> extends ResultState<T> {
+  final String message;
+  Error(this.message);
+}
