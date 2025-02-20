d5d8e31698d94d5e9ede2d7393e0941f733ca7d1
FauzyMaulana <beanonym04@gmail.com>
2025-02-20 12:02:14 +0700

    remove comment

--- lib/presentation/dashboard/widgets/movie_horizontal.dart
+++ lib/presentation/dashboard/widgets/movie_horizontal.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:watch_me/core/config/theme/app_colors.dart';
-import 'package:watch_me/core/config/theme/app_theme.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:watch_me/core/config/theme/app_typography.dart';
 import 'package:watch_me/presentation/dashboard/widgets/card_horizontal_movie.dart';
 import 'package:watch_me/presentation/dashboard/widgets/circular_border_button.dart';
-import 'package:watch_me/presentation/dashboard/widgets/rating_movie.dart';
 
 import '../../../core/utilities/result_state.dart';
 import '../../../domain/entities/base_paginate.dart';
@@ -17,7 +14,6 @@ class MovieHorizontal extends ConsumerStatefulWidget{
   final String title;
   final int page;
   final Function()? onSeeAllPressed;
-  // final AutoDisposeFutureProviderFamily<ResultState<BasePaginate<MovieEntity>>?, int> provider;
   final StateNotifierProvider<MovieViewmodel, ResultState<BasePaginate<MovieEntity>>> movieProvider;
   const MovieHorizontal({
     required this.title,
@@ -31,152 +27,6 @@ class MovieHorizontal extends ConsumerStatefulWidget{
   ConsumerState<ConsumerStatefulWidget> createState() {
     return _MovieHorizontalState();
   }
-
-  // @override
-  // Widget build(BuildContext context, WidgetRef ref) {
-
-  //   final state = ref.watch(movieProvider);
-
-  //   return Column(
-  //     crossAxisAlignment: CrossAxisAlignment.start,
-  //     children: [
-  //       Padding(
-  //         padding: const EdgeInsets.symmetric(horizontal: 20.0),
-  //         child: Row(
-  //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //           children: [
-  //             Text(
-  //               title,
-  //               style: Theme.of(context).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold)
-  //             ),
-
-  //             CircularBorderButton(
-  //               paddingHorizontal: 10,
-  //               paddingVertical: 10, 
-  //               label: "Lihat semua", 
-  //               textStyle: AppTypography.typeTypograph.labelSmall?.copyWith(
-  //                 color: AppColors.primary
-  //               ), 
-  //               onPressed: () {}
-  //             )
-  //           ],
-  //         ),
-  //       ),
-  //       SizedBox(height: 5.0),
-  //       state.when(
-  //         data: (resultState) {
-  //           if (resultState is Success<BasePaginate<MovieEntity>>) {
-  //             debugPrint("Data: ${resultState.data.results?.toList()}");
-  //             return SizedBox(
-  //               height: 250,
-  //               child: ListView.builder(
-  //                 scrollDirection: Axis.horizontal,
-  //                 itemCount: (resultState.data.results?.length ?? 0).clamp(0, 6),
-  //                 itemBuilder: (context, index) {
-  //                   final movie = resultState.data.results?[index];
-  //                   debugPrint("Movie: ${movie.toString()}");
-  //                   return Container(
-  //                     margin: EdgeInsets.only(left: 20.0),
-  //                     width: 143.0,
-  //                     child: Column(
-  //                       crossAxisAlignment: CrossAxisAlignment.start,
-  //                       children: [
-  //                         Expanded(
-  //                           child: ClipRRect(
-  //                             borderRadius: BorderRadius.circular(10.0),
-  //                             child: CachedNetworkImage(
-  //                               imageUrl: 'https://image.tmdb.org/t/p/original${movie?.posterPath ?? ""}',
-  //                               fit: BoxFit.cover,
-  //                               height: 212,
-  //                               width: 143,
-  //                               placeholder: (context, url) => Center(child: CircularProgressIndicator()),
-  //                               errorWidget: (context, url, error) => Container(
-  //                                 height: 212,
-  //                                 width: 143,
-  //                                 decoration: BoxDecoration(
-  //                                   borderRadius: BorderRadius.circular(10.0),
-  //                                   color: Colors.grey[300],
-  //                                 ),
-  //                                 child: Center(
-  //                                   child: Icon(Icons.wifi_off, color: Colors.red, size: 40.0),
-  //                                 ),
-  //                               ),
-  //                             ),
-  //                             // child: Image
-  //                             // .network(
-  //                             //   'https://image.tmdb.org/t/p/original${movie?.posterPath ?? ""}',
-  //                             //   fit: BoxFit.cover,
-  //                             //   height: 212,
-  //                             //   width: 143,
-  //                             //   loadingBuilder: (context, child, loadingProgress) => loadingProgress == null
-  //                             //     ? child
-  //                             //     : Center(
-  //                             //         child: CircularProgressIndicator(
-  //                             //           value: loadingProgress.expectedTotalBytes != null
-  //                             //               ? loadingProgress.cumulativeBytesLoaded / (loadingProgress.expectedTotalBytes ?? 1)
-  //                             //               : null,
-  //                             //         ),
-  //                             //     ),
-  //                             //   errorBuilder: (context, error, stackTrace) => Container(
-  //                             //     height: 212,
-  //                             //     width: 143,
-  //                             //     decoration: BoxDecoration(
-  //                             //       borderRadius: BorderRadius.circular(10.0),
-  //                             //       color: Colors.grey[300],
-  //                             //     ),
-  //                             //     child: Center(
-  //                             //       child: Icon(Icons.wifi_off, color: Colors.red, size: 40.0),
-  //                             //     ),
-  //                             //   ),
-  //                             // ),
-  //                           ),
-  //                         ),
-  //                         SizedBox(height: 12.0),
-  //                         Text(
-  //                           movie?.title ?? "",
-  //                           maxLines: 2,
-  //                           textAlign: TextAlign.start,
-  //                           style: AppTypography.typeTypograph.labelLarge?.copyWith(
-  //                             fontWeight: FontWeight.bold
-  //                           ),
-  //                         ),
-  //                         SizedBox(height: 6.0),
-  //                         Row(
-  //                           children: [
-  //                             Icon(Icons.star, color: AppColors.yellow, size: 16.0),
-  //                             SizedBox(width: 5.0),
-  //                             Text(
-  //                               "${movie?.voteAverage ?? 0.0}/10 IMDB",
-  //                               style: AppTypography.typeTypograph.labelSmall?.copyWith(
-  //                                 fontWeight: FontWeight.w400,
-  //                                 color: Colors.grey[600]
-  //                               ),
-  //                             ),
-  //                           ],
-  //                         )
-  //                       ],
-  //                     ),
-  //                   );
-  //                 },
-  //               ),
-  //             );
-  //           }
-  //           else if (resultState is Error<BasePaginate<MovieEntity>>) {
-  //             return Center(child: Text(resultState.message.toString()));
-  //           }
-  //           else if (resultState is Loading<BasePaginate<MovieEntity>>) {
-  //             return Center(child: CircularProgressIndicator());
-  //           }
-  //           else {
-  //             return Center(child: Text("Unknown State"));
-  //           }
-  //         },
-  //         loading: () => Center(child: CircularProgressIndicator()),
-  //         error: (error, stackTrace) => Center(child: Text(error.toString())),
-  //       ),
-  //     ],
-  //   );
-  // }
 }
 
 class _MovieHorizontalState extends ConsumerState<MovieHorizontal> {
@@ -205,108 +55,6 @@ class _MovieHorizontalState extends ConsumerState<MovieHorizontal> {
         _buildMovieList(),
       ],
     );
-
-
-
-    // Widget content;
-
-    // if (movieState is Success<BasePaginate<MovieEntity>>) {
-    //   debugPrint("Data: ${movieState.data.results?.toList()}");
-
-    //   content = SizedBox(
-    //     height: 250,
-    //     child: ListView.builder(
-    //       scrollDirection: Axis.horizontal,
-    //       itemCount: (movieState.data.results?.length ?? 0).clamp(0, 6),
-    //       itemBuilder: (context, index) {
-    //         final movie = movieState.data.results?[index];
-    //         debugPrint("Movie: ${movie.toString()}");
-
-    //         return Container(
-    //           margin: EdgeInsets.only(left: 20.0),
-    //           width: 143.0,
-    //           child: Column(
-    //             crossAxisAlignment: CrossAxisAlignment.start,
-    //             children: [
-    //               Expanded(
-    //                 child: ClipRRect(
-    //                   borderRadius: BorderRadius.circular(10.0),
-    //                   child: CachedNetworkImage(
-    //                     imageUrl: 'https://image.tmdb.org/t/p/original${movie?.posterPath ?? ""}',
-    //                     fit: BoxFit.cover,
-    //                     height: 212,
-    //                     width: 143,
-    //                     placeholder: (context, url) => Center(child: CircularProgressIndicator()),
-    //                     errorWidget: (context, url, error) => Container(
-    //                       height: 212,
-    //                       width: 143,
-    //                       decoration: BoxDecoration(
-    //                         borderRadius: BorderRadius.circular(10.0),
-    //                         color: Colors.grey[300],
-    //                       ),
-    //                       child: Center(
-    //                         child: Icon(Icons.wifi_off, color: Colors.red, size: 40.0),
-    //                       ),
-    //                     ),
-    //                   ),
-    //                 ),
-    //               ),
-    //               SizedBox(height: 12.0),
-    //               Text(
-    //                 movie?.title ?? "",
-    //                 maxLines: 2,
-    //                 textAlign: TextAlign.start,
-    //                 style: AppTypography.typeTypograph.labelLarge?.copyWith(
-    //                   fontWeight: FontWeight.bold
-    //                 ),
-    //               ),
-    //               SizedBox(height: 6.0),
-    //               RatingMovie(rating: movie?.voteAverage ?? 0.0),
-    //             ],
-    //           ),
-    //         );
-    //       },
-    //     ),
-    //   );
-    // } 
-    // else if (movieState is Error<BasePaginate<MovieEntity>>) {
-    //   content = Center(child: Text(movieState.message.toString()));
-    // } 
-    // else if (movieState is Loading<BasePaginate<MovieEntity>>) {
-    //   content = Center(child: CircularProgressIndicator());
-    // } 
-    // else {
-    //   content = Center(child: Text("Unknown State"));
-    // }
-
-    // return Column(
-    //   crossAxisAlignment: CrossAxisAlignment.start,
-    //   children: [
-    //     Padding(
-    //       padding: const EdgeInsets.symmetric(horizontal: 20.0),
-    //       child: Row(
-    //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //         children: [
-    //           Text(
-    //             widget.title,
-    //             style: Theme.of(context).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold)
-    //           ),
-    //           CircularBorderButton(
-    //             paddingHorizontal: 10,
-    //             paddingVertical: 10, 
-    //             label: "Lihat semua", 
-    //             textStyle: AppTypography.typeTypograph.labelSmall?.copyWith(
-    //               color: AppColors.primary
-    //             ), 
-    //             onPressed: () {}
-    //           )
-    //         ],
-    //       ),
-    //     ),
-    //     SizedBox(height: 5.0),
-    //     content, // Memastikan tidak ada return di tengah Column
-    //   ],
-    // );
   }
 
   Widget _buildHeader() {
@@ -352,35 +100,6 @@ class _MovieHorizontalState extends ConsumerState<MovieHorizontal> {
         else {
           return Center(child: Text("Unknown State"));
         }
-
-        // return Column(
-        //   crossAxisAlignment: CrossAxisAlignment.start,
-        //   children: [
-        //     Padding(
-        //       padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        //       child: Row(
-        //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //         children: [
-        //           Text(
-        //             widget.title,
-        //             style: Theme.of(context).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold)
-        //           ),
-        //           CircularBorderButton(
-        //             paddingHorizontal: 10,
-        //             paddingVertical: 10, 
-        //             label: "Lihat semua", 
-        //             textStyle: AppTypography.typeTypograph.labelSmall?.copyWith(
-        //               color: AppColors.primary
-        //             ), 
-        //             onPressed: () {}
-        //           )
-        //         ],
-        //       ),
-        //     ),
-        //     SizedBox(height: 5.0),
-        //     content,
-        //   ],
-        // );
       },
     );
   }
